27cd859f7f1b4a7fa6e5831a681b20db922b2fe5
Geoff Pado <geoff@pado.name>
2019-03-31 19:07:50 -0700

    Initial Commit

---
+++ Highlighter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Highlighter
+//
+//  Created by Geoff Pado on 3/31/19.
+//  Copyright © 2019 Cocoatype, LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ HighlighterTests/HighlighterTests.swift
@@ -0,0 +1,34 @@
+//
+//  HighlighterTests.swift
+//  HighlighterTests
+//
+//  Created by Geoff Pado on 3/31/19.
+//  Copyright © 2019 Cocoatype, LLC. All rights reserved.
+//
+
+import XCTest
+@testable import Highlighter
+
+class HighlighterTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
