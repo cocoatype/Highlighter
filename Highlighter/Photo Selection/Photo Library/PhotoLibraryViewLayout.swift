4f38f4ae848b36f185bb5a0295164be18ebc8dcf
Geoff Pado <geoff@pado.name>
2019-05-25 19:56:14 -0700

    Fix layout of photo album cells in photo picker

--- Highlighter/Photo Selection/Photo Library/PhotoLibraryViewLayout.swift
+++ Highlighter/Photo Selection/Photo Library/PhotoLibraryViewLayout.swift
@@ -11,23 +11,45 @@ class PhotoLibraryViewLayout: UICollectionViewFlowLayout {
     override func prepare() {
         super.prepare()
 
-        minimumInteritemSpacing = 1.0
-        itemSize = PhotoLibraryViewLayout.itemSize(for: collectionView?.traitCollection)
+        guard let collectionView = collectionView else { return }
+
+        let sideMargins = hasSideMargins ? CGFloat(15) : CGFloat.zero
+        let totalWidth = collectionView.bounds.width
+        let contentWidth = totalWidth - (sideMargins * 2)
+        let maxItemWidth = floor(contentWidth / CGFloat(numberOfItemsPerRow))
+        let totalSpacing = contentWidth - (maxItemWidth * CGFloat(numberOfItemsPerRow))
+        let maxSpacing = totalSpacing / (CGFloat(numberOfItemsPerRow) - 1)
+
+        sectionInset = UIEdgeInsets(top: 0, left: sideMargins, bottom: 0, right: sideMargins)
+        minimumInteritemSpacing = maxSpacing
+        minimumLineSpacing = maxSpacing
+        itemSize = CGSize(width: maxItemWidth, height: maxItemWidth)
+    }
+
+    private var hasSideMargins: Bool {
+        guard let traitCollection = collectionView?.traitCollection else { return false }
+        guard traitCollection.userInterfaceIdiom == .pad else { return false }
+        return traitCollection.horizontalSizeClass == .regular
+    }
+
+    private var numberOfItemsPerRow: Int {
+        guard let collectionView = collectionView else { return 4 }
+        let horizontalSizeClass = collectionView.traitCollection.horizontalSizeClass
+        let width = collectionView.bounds.width
+
+        switch horizontalSizeClass {
+        case .compact:
+            return width > PhotoLibraryViewLayout.compactSplitWidth ? 6 : 4
+        case .regular:
+            return width > PhotoLibraryViewLayout.regularSplitWidth ? 7 : 5
+        default: return 4
+        }
     }
 
     // MARK: Boilerplate
 
-    private static let smallCellSize = CGSize(width: 74.0, height: 74.0)
-    private static let largeCellSize = CGSize(width: 190.0, height: 190.0)
-
-    private static func itemSize(for traitCollection: UITraitCollection?) -> CGSize {
-        let horizontalSizeClass = traitCollection?.horizontalSizeClass ?? .unspecified
-        let verticalSizeClass = traitCollection?.verticalSizeClass ?? .unspecified
-
-        if (horizontalSizeClass, verticalSizeClass) == (.regular, .regular) {
-            return largeCellSize
-        } else { return smallCellSize }
-    }
+    private static let compactSplitWidth = CGFloat(500)
+    private static let regularSplitWidth = CGFloat(700)
 
     @available(*, unavailable)
     required init(coder: NSCoder) {
