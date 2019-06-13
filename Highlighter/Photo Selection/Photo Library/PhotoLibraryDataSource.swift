51756c44e0ff10dc08eb5aa39dc64ee30cfe7f68
Geoff Pado <geoff@pado.name>
2019-06-12 19:57:41 -0700

    Reload photo selector when photo library changes

--- Highlighter/Photo Selection/Photo Library/PhotoLibraryDataSource.swift
+++ Highlighter/Photo Selection/Photo Library/PhotoLibraryDataSource.swift
@@ -4,7 +4,16 @@
 import Photos
 import UIKit
 
-class PhotoLibraryDataSource: NSObject, UICollectionViewDataSource {
+class PhotoLibraryDataSource: NSObject, UICollectionViewDataSource, PHPhotoLibraryChangeObserver {
+    override init() {
+        super.init()
+        PHPhotoLibrary.shared().register(self)
+    }
+
+    weak var libraryView: PhotoLibraryView?
+
+    // MARK: Data Source
+
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return allPhotos.count
     }
@@ -22,13 +31,46 @@ class PhotoLibraryDataSource: NSObject, UICollectionViewDataSource {
 
     // MARK: Photos
 
-    private lazy var allPhotos: PHFetchResult<PHAsset> = {
-        let fetchOptions = PHFetchOptions()
-        fetchOptions.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: false)]
-        return PHAsset.fetchAssets(with: fetchOptions)
-    }()
+    private lazy var allPhotos: PHFetchResult<PHAsset> = self.fetchAllPhotos()
 
     func photo(at indexPath: IndexPath) -> PHAsset {
         return allPhotos[indexPath.item]
+    }
+
+    private func fetchAllPhotos() -> PHFetchResult<PHAsset> {
+        let fetchOptions = PHFetchOptions()
+        fetchOptions.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: false)]
+        return PHAsset.fetchAssets(with: fetchOptions)
+    }
+
+    // MARK: Photo Library Changes
+
+    func photoLibraryDidChange(_ changeInstance: PHChange) {
+        if let changes = changeInstance.changeDetails(for: allPhotos) {
+            allPhotos = changes.fetchResultAfterChanges
+
+            DispatchQueue.main.async { [weak self] in
+                guard let dataSource = self, let libraryView = dataSource.libraryView else { return }
+
+                if changes.hasIncrementalChanges {
+                    if let removed = changes.removedIndexes {
+                        libraryView.deleteItems(at: removed.map { IndexPath(item: $0, section:0) })
+                    }
+                    if let inserted = changes.insertedIndexes {
+                        libraryView.insertItems(at: inserted.map { IndexPath(item: $0, section:0) })
+                    }
+                    if let changed = changes.changedIndexes {
+                        libraryView.reloadItems(at: changed.map { IndexPath(item: $0, section:0) })
+                    }
+
+                    changes.enumerateMoves { fromIndex, toIndex in
+                        libraryView.moveItem(at: IndexPath(item: fromIndex, section: 0),
+                                                to: IndexPath(item: toIndex, section: 0))
+                    }
+                } else {
+                    libraryView.reloadData()
+                }
+            }
+        }
     }
 }
