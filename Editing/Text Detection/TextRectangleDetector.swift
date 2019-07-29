3402b9826cb105ad74042082a008d384beeaaf30
Geoff Pado <geoff@pado.name>
2019-07-29 15:22:26 -0700

    Run text detection engine and return rectangles

--- Editing/Text Detection/TextRectangleDetector.swift
+++ Editing/Text Detection/TextRectangleDetector.swift
@@ -1,6 +1,7 @@
 //  Created by Geoff Pado on 4/22/19.
 //  Copyright © 2019 Cocoatype, LLC. All rights reserved.
 
+import Vision
 import UIKit
 
 public class TextRectangleDetector: NSObject {
@@ -16,6 +17,24 @@ public class TextRectangleDetector: NSObject {
         }
 
         operationQueue.addOperation(detectionOperation)
+
+        if #available(iOSApplicationExtension 13.0, *), let recognitionOperation = TextRecognitionOperation(image: image) {
+            recognitionOperation.completionBlock = { [weak recognitionOperation] in
+                guard let recognizedTextObservations = recognitionOperation?.detectedTextResults else { return }
+                let appStrings = recognizedTextObservations.compactMap { observation -> VNRecognizedText? in
+                    guard let text = observation.topCandidates(1).first, text.string.contains("app") else { return nil }
+                    return text
+
+                }.compactMap { (text: VNRecognizedText) -> VNRectangleObservation? in
+                    guard let range = text.string.range(of: "app") else { return nil }
+                    return try? text.boundingBox(for: range)
+                }
+
+                dump(appStrings)
+            }
+
+            operationQueue.addOperation(recognitionOperation)
+        }
     }
 
     // MARK: Boilerplate
