cff8002821e6fe748069b02a2751009dd08fe798
Geoff Pado <geoff@pado.name>
2019-08-17 23:46:38 -0700

    Decrypt and validate signature on receipts

---
+++ Carthage/Checkouts/OpenSSL/OpenSSL-Universal.podspec
@@ -0,0 +1,49 @@
+Pod::Spec.new do |s|
+  s.name         = "OpenSSL-Universal"
+  s.version      = "1.0.2.#{("a".."z").to_a.index 's'}"
+  s.summary      = "OpenSSL for iOS and OS X"
+  s.description  = "OpenSSL is an SSL/TLS and Crypto toolkit. Deprecated in Mac OS and gone in iOS, this spec gives your project non-deprecated OpenSSL support. Supports OSX and iOS including Simulator (armv7,armv7s,arm64,x86_64)."
+  s.homepage     = "http://krzyzanowskim.github.io/OpenSSL/"
+  s.license	     = { :type => 'OpenSSL (OpenSSL/SSLeay)', :file => 'LICENSE.txt' }
+  s.source       = { :git => "https://github.com/krzyzanowskim/OpenSSL.git", :tag => "#{s.version}" }
+
+  s.authors       =  {'Mark J. Cox' => 'mark@openssl.org',
+                     'Ralf S. Engelschall' => 'rse@openssl.org',
+                     'Dr. Stephen Henson' => 'steve@openssl.org',
+                     'Ben Laurie' => 'ben@openssl.org',
+                     'Lutz Jänicke' => 'jaenicke@openssl.org',
+                     'Nils Larsch' => 'nils@openssl.org',
+                     'Richard Levitte' => 'nils@openssl.org',
+                     'Bodo Möller' => 'bodo@openssl.org',
+                     'Ulf Möller' => 'ulf@openssl.org',
+                     'Andy Polyakov' => 'appro@openssl.org',
+                     'Geoff Thorpe' => 'geoff@openssl.org',
+                     'Holger Reif' => 'holger@openssl.org',
+                     'Paul C. Sutton' => 'geoff@openssl.org',
+                     'Eric A. Young' => 'eay@cryptsoft.com',
+                     'Tim Hudson' => 'tjh@cryptsoft.com',
+                     'Justin Plouffe' => 'plouffe.justin@gmail.com'}
+
+  s.ios.deployment_target = '6.0'
+  s.ios.source_files        = 'ios/include/openssl/**/*.h'
+  s.ios.public_header_files = 'ios/include/openssl/**/*.h'
+  s.ios.header_dir          = 'openssl'
+  s.ios.preserve_paths      = 'ios/lib/libcrypto.a', 'ios/lib/libssl.a'
+  s.ios.vendored_libraries  = 'ios/lib/libcrypto.a', 'ios/lib/libssl.a'
+
+  s.osx.deployment_target = '10.9'
+  s.osx.source_files        = 'macos/include/openssl/**/*.h'
+  s.osx.public_header_files = 'macos/include/openssl/**/*.h'
+  s.osx.header_dir          = 'openssl'
+  s.osx.preserve_paths      = 'macos/lib/libcrypto.a', 'macos/lib/libssl.a'
+  s.osx.vendored_libraries  = 'macos/lib/libcrypto.a', 'macos/lib/libssl.a'
+
+  s.requires_arc = false
+
+  s.subspec 'Framework' do |sp|
+    sp.ios.deployment_target = '8.0'
+    sp.ios.vendored_frameworks = 'Frameworks/ios/OpenSSL.framework'
+    sp.osx.deployment_target = '10.9'
+    sp.osx.vendored_frameworks = 'Frameworks/macos/OpenSSL.framework'
+  end
+end
--- Highlighter/Purchasing/ReceiptValidator.swift
+++ Highlighter/Purchasing/ReceiptValidator.swift
@@ -2,15 +2,62 @@
 //  Copyright © 2019 Cocoatype, LLC. All rights reserved.
 
 import Foundation
+import OpenSSL
 
 class ReceiptValidator {
     var appReceiptStatus: ReceiptStatus { return .valid }
     var unlockPurchaseStatus: ReceiptStatus {
-        guard FileManager.default.fileExists(atPath: ReceiptValidator.receiptURL.path) else { return .missing }
-        return .valid
+        do {
+            let receiptData = try Data(contentsOf: ReceiptValidator.receiptURL)
+            guard let container = decrypt(receiptData) else { return .invalid }
+            guard validate(container) else { return .invalid }
+            return .valid
+        } catch {
+            return .missing
+        }
+    }
+
+    private func decrypt(_ data: Data) -> UnsafeMutablePointer<PKCS7>? {
+        let receiptBIO = BIO_new(BIO_s_mem())
+        BIO_write(receiptBIO, (data as NSData).bytes, Int32(data.count))
+
+        let container = d2i_PKCS7_bio(receiptBIO, nil)
+        guard container != nil else { return nil }
+
+        let asn1Obj = container?.pointee.d.sign.pointee.contents.pointee.type
+        let dataTypeCode = OBJ_obj2nid(asn1Obj)
+
+        guard dataTypeCode == NID_pkcs7_data else { return nil }
+        return container
+    }
+
+    private func validate(_ container: UnsafeMutablePointer<PKCS7>) -> Bool {
+        guard OBJ_obj2nid(container.pointee.type) == NID_pkcs7_signed else { return false }
+
+        let appleCertificateBIO = BIO_new(BIO_s_mem())
+        let appleCertificateData = ReceiptValidator.appleCertificateData
+        BIO_write(appleCertificateBIO, (appleCertificateData as NSData).bytes, Int32(appleCertificateData.count))
+        guard let appleCertificate = d2i_X509_bio(appleCertificateBIO, nil) else { return false }
+
+        let certificateStore = X509_STORE_new()
+        X509_STORE_add_cert(certificateStore, appleCertificate)
+
+        OpenSSL_add_all_digests()
+
+        let result = PKCS7_verify(container, nil, certificateStore, nil, nil, 0)
+        return result == 1
     }
 
     // MARK: Boilerplate
+
+    private static let appleCertificateData: Data = {
+        guard let dataURL = Bundle.main.url(forResource: "AppleIncRootCertificate", withExtension: "cer"), let data = try? Data(contentsOf: dataURL) else {
+            fatalError("Error locating Apple root certificate data")
+        }
+
+        return data
+    }()
+
     private static let receiptURL: URL = {
         guard let receiptURL = Bundle.main.appStoreReceiptURL else {
             fatalError("Error locating app receipt")
@@ -24,4 +71,10 @@ enum ReceiptStatus {
     case valid
     case invalid
     case missing
+}
+
+extension Data {
+    var hex: String {
+        return map { String(format: "%02hhx", $0) }.joined()
+    }
 }
