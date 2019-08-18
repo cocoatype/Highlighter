a618684f21a606f061b5aae663585b60f636783e
Geoff Pado <geoff@pado.name>
2019-08-17 22:50:31 -0700

    Add button to make purchase

--- Highlighter/Settings/Presented View Controllers/Purchase Marketing/PurchaseMarketingView.swift
+++ Highlighter/Settings/Presented View Controllers/Purchase Marketing/PurchaseMarketingView.swift
@@ -8,19 +8,27 @@ class PurchaseMarketingView: UIView {
         super.init(frame: .zero)
         backgroundColor = .primary
 
+        purchaseButton.addTarget(nil, action: #selector(SettingsNavigationController.startPurchase), for: .primaryActionTriggered)
+
         addSubview(label)
+        addSubview(purchaseButton)
+
         NSLayoutConstraint.activate([
             label.topAnchor.constraint(equalTo: topAnchor, constant: 20.0),
             label.leadingAnchor.constraint(equalTo: readableContentGuide.leadingAnchor),
-            label.trailingAnchor.constraint(equalTo: readableContentGuide.trailingAnchor)
+            label.trailingAnchor.constraint(equalTo: readableContentGuide.trailingAnchor),
+            purchaseButton.leadingAnchor.constraint(equalTo: label.leadingAnchor),
+            purchaseButton.topAnchor.constraint(equalTo: label.bottomAnchor, constant: 26.0)
         ])
     }
 
     // MARK: Boilerplate
 
     private static let marketingText = NSLocalizedString("PurchaseMarketingView.marketingText", comment: "Marketing text for the purchase marketing view")
+    private static let purchaseButtonTitle = NSLocalizedString("PurchaseMarketingView.purchaseButtonTitleWithoutProduct", comment: "Title for the purchase button on the purchase marketing view")
 
     private let label = PurchaseMarketingViewLabel(text: PurchaseMarketingView.marketingText)
+    private let purchaseButton = PromptButton(title: PurchaseMarketingView.purchaseButtonTitle)
 
     @available(*, unavailable)
     required init(coder: NSCoder) {
