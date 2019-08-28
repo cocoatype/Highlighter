bfdf85976720a4ead93bf810988cbaf41a903383
Geoff Pado <geoff@pado.name>
2019-08-28 00:12:43 -0700

    Weak link PencilKit to fix crash on iOS 12
    This required rewriting the PencilKit code in Obj-C, because you can't
    weak-link the libSwift dylibs. Yay!

---
+++ Canvas/PhotoEditingCanvasView.m
@@ -0,0 +1,60 @@
+//  Created by Geoff Pado on 8/27/19.
+//  Copyright © 2019 Cocoatype, LLC. All rights reserved.
+
+#import "PhotoEditingCanvasView.h"
+#import "PhotoEditingCanvasBrushStrokeView.h"
+
+#define STANDARD_LINE_WIDTH 10.0
+
+@implementation PhotoEditingCanvasView
+
+- (instancetype)init
+{
+    if ((self = [super initWithFrame:CGRectZero])) {
+        self.allowsFingerDrawing = YES;
+        self.backgroundColor = [UIColor clearColor];
+        self.opaque = NO;
+        self.tool = [[PKInkingTool alloc] initWithInkType:PKInkTypeMarker color:[UIColor blackColor] width:STANDARD_LINE_WIDTH];
+        self.translatesAutoresizingMaskIntoConstraints = NO;
+    }
+
+    return self;
+}
+
+- (CGFloat)currentLineWidth {
+    return [((PKInkingTool *)[self tool]) width];
+}
+
+- (void)updateToolWithCurrentZoomScale:(CGFloat)currentZoomScale {
+    self.tool = [[PKInkingTool alloc] initWithInkType:PKInkTypeMarker color:[UIColor blackColor] width:[self adjustedLineWidthForZoomScale:currentZoomScale]];
+}
+
+- (CGFloat)adjustedLineWidthForZoomScale:(CGFloat)zoomScale {
+    return STANDARD_LINE_WIDTH * pow(zoomScale, -1.0);
+}
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    [super touchesBegan:touches withEvent:event];
+    [self.brushStrokeView touchesBegan:touches withEvent:event];
+}
+
+- (void)touchesMoved:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    [super touchesMoved:touches withEvent:event];
+    [self.brushStrokeView touchesMoved:touches withEvent:event];
+}
+
+- (void)touchesCancelled:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    [super touchesCancelled:touches withEvent:event];
+    [self.brushStrokeView touchesCancelled:touches withEvent:event];
+}
+
+- (void)touchesEnded:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    [super touchesEnded:touches withEvent:event];
+    [self.brushStrokeView touchesEnded:touches withEvent:event];
+}
+
+- (PhotoEditingCanvasBrushStrokeView *)brushStrokeView {
+    return (PhotoEditingCanvasBrushStrokeView *)[self superview];
+}
+
+@end
