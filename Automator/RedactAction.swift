c674d4957539450d9eff62e57ef75e494e6f6ec7
Geoff Pado <geoff@pado.name>
2020-10-28 20:14:09 -0700

    Export a correct image output

--- Automator/RedactAction.swift
+++ Automator/RedactAction.swift
@@ -5,9 +5,9 @@ import AppKit
 import Automator
 import Redacting
 import os.log
+import UniformTypeIdentifiers
 
 class RedactAction: AMBundleAction {
-//    override func run(withInput input: Any?) throws -> Any {
     override func runAsynchronously(withInput input: Any?) {
         guard let inputArray = input as? [Any] else { return finish(with: ActionError.unknownInput, originalInput: input) }
 
@@ -30,25 +30,21 @@ class RedactAction: AMBundleAction {
             guard let observations = observations else { return self?.finishRunningWithError(nil) ?? () }
             let matchingObservations = observations.filter { $0.string == "Highlighter" }
 
-            let finalImage = NSImage(size: firstImage.size)
-            finalImage.lockFocus()
-            if let context = NSGraphicsContext.current?.cgContext {
-                var imageRect = NSRect(origin: .zero, size: firstImage.size)
-                if let cgImage = firstImage.cgImage(forProposedRect: &imageRect, context: nil, hints: nil) {
-                    context.draw(cgImage, in: imageRect)
+            let finalImage = NSImage(size: firstImage.size, flipped: false) { rect -> Bool in
+                var rect = rect
+                guard let context = NSGraphicsContext.current?.cgContext else { return false }
+
+                if let cgImage = firstImage.cgImage(forProposedRect: &rect, context: nil, hints: nil) {
+                    context.draw(cgImage, in: rect)
                 }
 
                 let rects = matchingObservations.map(\.bounds)
                 context.setFillColor(NSColor.systemRed.cgColor)
                 context.fill(rects)
+                return true
             }
-            finalImage.unlockFocus()
 
-            var inputDump = String()
-            dump(matchingObservations, to: &inputDump)
-            os_log("foboar %{public}@", inputDump)
-
-            self?.finish(with: finalImage)
+            self?.finish(with: finalImage, for: inputItems[0])
         }
     }
 
@@ -57,11 +53,21 @@ class RedactAction: AMBundleAction {
         finishRunningWithError(error)
     }
 
-    private func finish(with image: NSImage) {
+    private func finish(with image: NSImage, for input: Input) {
         operationQueue.addOperation { [weak self] in
             do {
-                let writeURL = URL(fileURLWithPath: NSTemporaryDirectory()).appendingPathComponent(UUID().uuidString).appendingPathExtension("tiff")
-                try image.tiffRepresentation?.write(to: writeURL)
+                let writeURL = URL(fileURLWithPath: NSTemporaryDirectory()).appendingPathComponent(UUID().uuidString).appendingPathExtension(input.fileType?.preferredFilenameExtension ?? "png")
+
+                guard let imageRep = NSBitmapImageRep(bitmapDataPlanes: nil, pixelsWide: Int(image.size.width), pixelsHigh: Int(image.size.height), bitsPerSample: 8, samplesPerPixel: 4, hasAlpha: true, isPlanar: false, colorSpaceName: .deviceRGB, bytesPerRow: Int(image.size.width) * 4, bitsPerPixel: 32) else { throw ActionError.writeError }
+
+                let context = NSGraphicsContext(bitmapImageRep: imageRep)
+                NSGraphicsContext.saveGraphicsState()
+                NSGraphicsContext.current = context
+                image.draw(at: .zero, from: CGRect(origin: .zero, size: image.size), operation: .copy, fraction: 1)
+                NSGraphicsContext.restoreGraphicsState()
+
+                guard let data = imageRep.representation(using: input.imageType, properties: [:]) else { throw ActionError.writeError }
+                try data.write(to: writeURL)
 
                 self?.output = writeURL.path
                 self?.finishRunningWithError(nil)
@@ -84,6 +90,31 @@ enum Input {
          url(URL),
          data(Data)
 
+    var fileType: UTType? {
+        let fileURL: URL
+        switch self {
+        case .string(let string): fileURL = URL(fileURLWithPath: string)
+        case .url(let url): fileURL = url
+        default: return nil
+        }
+
+        do {
+            let resourceValues = try fileURL.resourceValues(forKeys: [.contentTypeKey])
+            return resourceValues.contentType
+        } catch { return nil }
+    }
+
+    var imageType: NSBitmapImageRep.FileType {
+        switch fileType {
+        case UTType.tiff: return .tiff
+        case UTType.bmp: return .bmp
+        case UTType.gif: return .gif
+        case UTType.jpeg: return .jpeg
+        case UTType.png: return .png
+        default: return .png
+        }
+    }
+
     var image: NSImage? {
         switch self {
         case .string(let string): return NSImage(contentsOfFile: string)
@@ -94,5 +125,6 @@ enum Input {
 }
 
 enum ActionError: Error {
+    case writeError
     case unknownInput
 }
