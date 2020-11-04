085fbcc42eeb4d79b554dd959bba42622dc2d3e8
Geoff Pado <geoff@pado.name>
2020-11-04 00:33:39 -0800

    Fix building Highlighter app

--- Editing/Extensions/UIBezierPathExtensions.swift
+++ Editing/Extensions/UIBezierPathExtensions.swift
@@ -1,7 +1,7 @@
 //  Created by Geoff Pado on 5/1/19.
 //  Copyright © 2019 Cocoatype, LLC. All rights reserved.
 
-#if canImport(AppKit)
+#if canImport(AppKit) && !targetEnvironment(macCatalyst)
 import AppKit
 
 extension NSBezierPath {
--- Editing/Redactions/TextObservationRedaction.swift
+++ Editing/Redactions/TextObservationRedaction.swift
@@ -1,7 +1,7 @@
 //  Created by Geoff Pado on 7/29/19.
 //  Copyright © 2019 Cocoatype, LLC. All rights reserved.
 
-#if canImport(AppKit)
+#if canImport(AppKit) && !targetEnvironment(macCatalyst)
 import AppKit
 
 public struct TextObservationRedaction: Redaction {
