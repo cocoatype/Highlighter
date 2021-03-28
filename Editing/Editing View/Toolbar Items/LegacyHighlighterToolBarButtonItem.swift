b1db7cb8e2eb07b02aa710eed0683359b78cd1e3
Geoff Pado <geoff@pado.name>
2021-03-28 02:06:56 -0700

    Add tool picker menu on iOS 14

---
+++ Editing/Editing View/Toolbar Items/LegacyHighlighterToolBarButtonItem.swift
@@ -0,0 +1,8 @@
+//  Created by Geoff Pado on 3/28/21.
+//  Copyright © 2021 Cocoatype, LLC. All rights reserved.
+
+import UIKit
+
+class LegacyHighlighterToolBarButtonItem: UIBarButtonItem {
+    
+}
