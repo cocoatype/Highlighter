38d06a46546c6f0ea47821d4a5bea8616a1e8a65
Geoff Pado <geoff@pado.name>
2021-04-17 00:52:27 -0700

    Fix macOS help menu

---
+++ Highlighter/Application/Help Menu/HelpMenuAcknowledgementsAction.swift
@@ -0,0 +1,23 @@
+//  Created by Geoff Pado on 4/17/21.
+//  Copyright © 2021 Cocoatype, LLC. All rights reserved.
+
+import UIKit
+
+#if targetEnvironment(macCatalyst)
+class HelpMenuAcknowledgementsAction: UIAction {
+    convenience init(void: Void = ()) {
+        self.init(title: Self.menuItemTitle) { _ in
+            UIApplication.shared.open(AcknowledgementsViewController.url, options: [:], completionHandler: nil)
+        }
+    }
+
+    // MARK: Boilerplate
+
+    private static let menuItemTitle = NSLocalizedString("SettingsContentProvider.Item.acknowledgements", comment: "Acknowledgements menu item title")
+
+    @available(*, unavailable)
+    required init(coder: NSCoder) {
+        fatalError("\(String(describing: type(of: Self.self))) does not implement init(coder:)")
+    }
+}
+#endif
