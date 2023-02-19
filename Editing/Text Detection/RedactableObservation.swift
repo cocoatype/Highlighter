76f63a316be11d066628fd7c90b96f8fb8c5d851
Geoff Pado <geoff@pado.name>
2023-02-19 08:49:57 -0800

    Add missing file

---
+++ Editing/Text Detection/RedactableObservation.swift
@@ -0,0 +1,8 @@
+//  Created by Geoff Pado on 1/20/23.
+//  Copyright © 2023 Cocoatype, LLC. All rights reserved.
+
+import Foundation
+
+protocol RedactableObservation {
+    var characterObservations: [CharacterObservation] { get }
+}
