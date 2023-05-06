a48763501a9e969b2cf81b24bd84179322979d5e
Geoff Pado <geoff@pado.name>
2023-05-05 17:33:58 -0700

    Add events for app ratings prompt

--- Modules/Capabilities/AppRatings/Sources/AppRatingsPrompter.swift
+++ Modules/Capabilities/AppRatings/Sources/AppRatingsPrompter.swift
@@ -2,28 +2,45 @@
 //  Copyright © 2019 Cocoatype, LLC. All rights reserved.
 
 import Defaults
+import ErrorHandling
+import Logging
 import Foundation
 import StoreKit
 
 public struct AppRatingsPrompter {
     public init() {
-        self.init(requestMethod: SKStoreReviewController.requestReview(in:))
+        self.init(logger: TelemetryLogger(), requestMethod: SKStoreReviewController.requestReview(in:))
     }
 
-    init(requestMethod: @escaping ((UIWindowScene) -> Void) = SKStoreReviewController.requestReview(in:)) {
+    init(logger: Logger, requestMethod: @escaping ((UIWindowScene) -> Void) = SKStoreReviewController.requestReview(in:)) {
+        self.logger = logger
         self.requestMethod = requestMethod
     }
 
     public func displayRatingsPrompt(in windowScene: UIWindowScene?) {
-        guard let windowScene = windowScene,
-              Defaults.numberOfSaves >= Self.minNumberOfSaves
+        guard let windowScene else {
+            ErrorHandler(logger: logger).log(AppRatingsError.missingWindowScene)
+            return
+        }
+
+        guard Defaults.numberOfSaves >= Self.minNumberOfSaves
         else { return }
 
         requestMethod(windowScene)
+        logger.log(Event(name: .requestedRating, info: [:]))
     }
 
     // MARK: Boilerplate
 
     private static let minNumberOfSaves = 3
+    private let logger: Logger
     private let requestMethod: (UIWindowScene) -> Void
+}
+
+private enum AppRatingsError: Error {
+    case missingWindowScene
+}
+
+extension Event.Name {
+    static let requestedRating = Event.Name("AppRatingsPrompter.requestedRating")
 }
--- Modules/Capabilities/AppRatings/Tests/AppRatingsPrompterTests.swift
+++ Modules/Capabilities/AppRatings/Tests/AppRatingsPrompterTests.swift
@@ -7,13 +7,14 @@ import TestHelpers
 import XCTest
 
 @testable import AppRatings
+@testable import Logging
 
 class AppRatingsPrompterTests: XCTestCase {
     func testDisplayingPromptOnFirstAttemptDoesNotPrompt() throws {
         Defaults.numberOfSaves = 1
         let promptExpectation = expectation(description: "prompted")
         promptExpectation.isInverted = true
-        let prompter = AppRatingsPrompter { _ in
+        let prompter = AppRatingsPrompter(logger: SpyLogger()) { _ in
             promptExpectation.fulfill()
         }
 
@@ -26,7 +27,7 @@ class AppRatingsPrompterTests: XCTestCase {
     func testDisplayingPromptOnThirdAttemptPrompts() throws {
         Defaults.numberOfSaves = 3
         let promptExpectation = expectation(description: "prompted")
-        let prompter = AppRatingsPrompter { _ in
+        let prompter = AppRatingsPrompter(logger: SpyLogger()) { _ in
             promptExpectation.fulfill()
         }
 
@@ -39,7 +40,7 @@ class AppRatingsPrompterTests: XCTestCase {
     func testDisplayingPromptOnFifthAttemptPrompts() throws {
         Defaults.numberOfSaves = 5
         let promptExpectation = expectation(description: "prompted")
-        let prompter = AppRatingsPrompter { _ in
+        let prompter = AppRatingsPrompter(logger: SpyLogger()) { _ in
             promptExpectation.fulfill()
         }
 
@@ -47,5 +48,35 @@ class AppRatingsPrompterTests: XCTestCase {
         prompter.displayRatingsPrompt(in: windowScene)
 
         waitForExpectations(timeout: 1)
+    }
+
+    func testDisplayingPromptWithNoWindowSceneLogsError() throws {
+        let spy = SpyLogger()
+        let prompter = AppRatingsPrompter(logger: spy) { _ in }
+
+        prompter.displayRatingsPrompt(in: nil)
+
+        let event = try XCTUnwrap(spy.loggedEvent)
+        XCTAssertEqual(event.value, "logError")
+        XCTAssertEqual(event.info, ["errorDescription": "missingWindowScene"])
+    }
+
+    func testDisplayingPromptLogsEvent() throws {
+        Defaults.numberOfSaves = 999
+        let spy = SpyLogger()
+        let prompter = AppRatingsPrompter(logger: spy) { _ in }
+        let windowScene = try InstanceHelper.create(UIWindowScene.self)
+
+        prompter.displayRatingsPrompt(in: windowScene)
+
+        let event = try XCTUnwrap(spy.loggedEvent)
+        XCTAssertEqual(event.value, "AppRatingsPrompter.requestedRating")
+    }
+}
+
+private class SpyLogger: Logger {
+    private(set) var loggedEvent: Event?
+    func log(_ event: Event) {
+        loggedEvent = event
     }
 }
