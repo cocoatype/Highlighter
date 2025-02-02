3296db0bdaea59ffc32fa576d411aa3db3b7bece
Geoff Pado <geoff@pado.name>
2025-02-02 02:53:02 -0800

    Update DebugOverlay, Observations, and Redactions to Swift 6.
    Closes #141.
    Closes #144.

--- Tuist/ProjectDescriptionHelpers/Targets/Capabilities/DebugOverlay.swift
+++ Tuist/ProjectDescriptionHelpers/Targets/Capabilities/DebugOverlay.swift
@@ -3,6 +3,7 @@ import ProjectDescription
 public enum DebugOverlay {
     public static let target = Target.capabilitiesTarget(
         name: "DebugOverlay",
+        usesMaxSwiftVersion: true,
         dependencies: [
             .target(Defaults.target),
             .target(DesignSystem.target),
