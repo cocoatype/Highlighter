7dac5a6b378db352218aafb5c86d567c2f06cdd8
Geoff Pado <geoff@pado.name>
2025-02-17 11:05:18 -0800

    Fix test compilation

--- Modules/Capabilities/SettingsUI/Tests/List/Content/SettingsContentPurchasedFeaturesSectionTests.swift
+++ Modules/Capabilities/SettingsUI/Tests/List/Content/SettingsContentPurchasedFeaturesSectionTests.swift
@@ -11,25 +11,25 @@ import XCTest
 @available(iOS 16.0, *)
 class SettingsContentPurchasedFeaturesSectionTests: XCTestCase {
     func testContainsPurchaseNavigationLinkIfNotPurchased() throws {
-        let section = try SettingsContentPurchasedFeaturesSection(state: .loading).inspect().find(SettingsContentPurchasedFeaturesSection.self)
+        let section = try SettingsContentPurchasedFeaturesSection(state: .constant(.loading)).inspect().find(SettingsContentPurchasedFeaturesSection.self)
 
-        XCTAssertNoThrow(try section.find(PurchaseNavigationLink<PurchaseMarketingView>.self))
+        XCTAssertNoThrow(try section.find(PurchaseNavigationLink.self))
     }
 
     func testDoesNotContainSettingsNavigationLinkIfNotPurchased() throws {
-        let section = try SettingsContentPurchasedFeaturesSection(state: .loading).inspect().find(SettingsContentPurchasedFeaturesSection.self)
+        let section = try SettingsContentPurchasedFeaturesSection(state: .constant(.loading)).inspect().find(SettingsContentPurchasedFeaturesSection.self)
 
         XCTAssertThrowsError(try section.find(SettingsNavigationLink<AutoRedactionsEditView>.self))
     }
 
     func testDoesNotContainPurchaseNavigationLinkIfPurchased() throws {
-        let section = try SettingsContentPurchasedFeaturesSection(state: .purchased).inspect().find(SettingsContentPurchasedFeaturesSection.self)
+        let section = try SettingsContentPurchasedFeaturesSection(state: .constant(.purchased)).inspect().find(SettingsContentPurchasedFeaturesSection.self)
 
-        XCTAssertThrowsError(try section.find(PurchaseNavigationLink<PurchaseMarketingView>.self))
+        XCTAssertThrowsError(try section.find(PurchaseNavigationLink.self))
     }
 
     func testContainsSettingsNavigationLinkIfPurchased() throws {
-        let section = try SettingsContentPurchasedFeaturesSection(state: .purchased).inspect().find(SettingsContentPurchasedFeaturesSection.self)
+        let section = try SettingsContentPurchasedFeaturesSection(state: .constant(.purchased)).inspect().find(SettingsContentPurchasedFeaturesSection.self)
 
         XCTAssertNoThrow(try section.find(SettingsNavigationLink<AutoRedactionsEditView>.self))
     }
