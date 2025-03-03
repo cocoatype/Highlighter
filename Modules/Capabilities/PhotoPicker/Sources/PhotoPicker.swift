68985a8a35b86fb27a746ba08eef790e6e937548
Geoff Pado <geoff@pado.name>
2025-03-03 06:56:22 -0800

    Revert "Add test coverage for PhotoPicker"
    This reverts commit 0a51d5512631ae9875f72d0e5ccba5df2ebe812f.

--- Modules/Capabilities/PhotoPicker/Sources/PhotoPicker.swift
+++ Modules/Capabilities/PhotoPicker/Sources/PhotoPicker.swift
@@ -21,15 +21,20 @@ public class PhotoPicker: NSObject, PHPickerViewControllerDelegate {
 
     // MARK: Delegate Methods
 
-    private let imageLoader = PhotoPickerResultImageLoader()
     public func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
         guard let delegate = delegate else { return }
-        guard let result = results.first else {
+        guard let provider = results.first?.itemProvider else {
             return delegate.picker(self, didSelectImage: nil)
         }
 
-        Task {
-            await delegate.picker(self, didSelectImage: imageLoader.loadImage(for: result))
+        provider.loadObject(ofClass: UIImage.self) { [weak self] loadedObject, _ in
+            guard let image = loadedObject as? UIImage else { return }
+            Task { [weak self] in
+                await MainActor.run { [weak self] in
+                    guard let picker = self, let delegate = picker.delegate else { return }
+                    delegate.picker(picker, didSelectImage: image)
+                }
+            }
         }
     }
 }
--- Modules/Capabilities/PhotoPicker/Tests/PhotoPickerTests.swift
+++ Modules/Capabilities/PhotoPicker/Tests/PhotoPickerTests.swift
@@ -1,44 +1,8 @@
 //  Created by Geoff Pado on 3/3/25.
 //  Copyright © 2025 Cocoatype, LLC. All rights reserved.
 
-import PhotosUI
 import Testing
-import UIKit
 
 @testable import PhotoPicker
 
-@MainActor
-struct PhotoPickerTests {
-    @Test
-    func pickerViewController() {
-        let picker = PhotoPicker()
-        let pickerViewController = picker.pickerViewController
-
-        #expect(pickerViewController.configuration.selectionLimit == 1)
-        #expect(pickerViewController.configuration.filter == .images)
-        #expect(pickerViewController.overrideUserInterfaceStyle == .dark)
-        #expect(pickerViewController.delegate === picker)
-    }
-
-    @Test
-    func didFinishPickingWithNoResults() async {
-        class StubDelegate: PhotoPickerDelegate {
-            private let confirmation: Confirmation
-            init(confirmation: Confirmation) {
-                self.confirmation = confirmation
-            }
-
-            func picker(_ picker: PhotoPicker, didSelectImage image: UIImage?) {
-                #expect(image == nil)
-                confirmation()
-            }
-        }
-
-        await confirmation { confirmation in
-            let picker = PhotoPicker()
-            let delegate = StubDelegate(confirmation: confirmation)
-            picker.delegate = delegate
-            picker.picker(picker.pickerViewController, didFinishPicking: [])
-        }
-    }
-}
+struct PhotoPickerTests {}
