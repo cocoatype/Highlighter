164387735ef3d0c2216464b2e6e1baaac3303a24
Geoff Pado <geoff@pado.name>
2025-07-04 19:29:43 -0700

    Update Detections to Swift 6

--- Tuist/ProjectDescriptionHelpers/Targets/Capabilities/Detections.swift
+++ Tuist/ProjectDescriptionHelpers/Targets/Capabilities/Detections.swift
@@ -5,6 +5,7 @@ public enum Detections {
         Target.capabilitiesTarget(
             name: "Detections",
             sdk: sdk,
+            usesMaxSwiftVersion: true,
             dependencies: [
                 .target(Observations.target(sdk: sdk)),
             ]
@@ -13,6 +14,7 @@ public enum Detections {
 
     public static let testTarget = Target.capabilitiesTestTarget(
         name: "Detections",
+        usesMaxSwiftVersion: true,
         dependencies: [
         ]
     )
