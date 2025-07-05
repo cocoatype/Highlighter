ff858127aa83609d033ee6374607f197d25b0cde
Geoff Pado <geoff@pado.name>
2025-07-04 19:12:29 -0700

    Update DesignSystem to Swift 6

--- Modules/Capabilities/DesignSystem/Sources/Appearance/AppearanceWriter.swift
+++ Modules/Capabilities/DesignSystem/Sources/Appearance/AppearanceWriter.swift
@@ -1,6 +1,6 @@
 //  Created by Geoff Pado on 5/30/24.
 //  Copyright © 2024 Cocoatype, LLC. All rights reserved.
 
-public protocol AppearanceWriter {
+@MainActor public protocol AppearanceWriter: Sendable {
     func overwriteAppearance()
 }
