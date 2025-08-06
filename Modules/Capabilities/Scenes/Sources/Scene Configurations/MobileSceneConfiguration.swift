64c3b7f5288deef0532229ef0f1ca5547a0b312f
Geoff Pado <geoff@pado.name>
2025-08-06 03:59:12 -0700

    Fix Open Recent displaying black screen
    This doesn't actually do anything yet, but at least the app doesn't
    outright break any more.

--- Modules/Capabilities/Scenes/Sources/Scene Configurations/MobileSceneConfiguration.swift
+++ Modules/Capabilities/Scenes/Sources/Scene Configurations/MobileSceneConfiguration.swift
@@ -1,8 +1,10 @@
 //  Created by Geoff Pado on 7/26/24.
 //  Copyright © 2024 Cocoatype, LLC. All rights reserved.
 
+#if !targetEnvironment(macCatalyst)
 class MobileSceneConfiguration: WindowSceneConfiguration {
     init() {
         super.init(name: "Highlighter")
     }
 }
+#endif
--- Modules/Capabilities/Scenes/Sources/SceneProvider.swift
+++ Modules/Capabilities/Scenes/Sources/SceneProvider.swift
@@ -9,16 +9,16 @@ public struct SceneProvider {
     public init() {}
 
     public func sceneConfiguration(session: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        #if targetEnvironment(macCatalyst)
         if options.userActivities.contains(where: { $0.activityType == DesktopSettingsActivity.activityType}) {
             return DesktopSettingsSceneConfiguration()
         } else if SceneDependencyWrangler().dependencies(from: session, options: options) != nil {
             return DesktopSceneConfiguration()
         } else {
-            #if targetEnvironment(macCatalyst)
             return DesktopOpenSceneConfiguration()
-            #else
-            return MobileSceneConfiguration()
-            #endif
         }
+        #else
+        return MobileSceneConfiguration()
+        #endif
     }
 }
